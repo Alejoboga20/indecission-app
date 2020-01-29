0233d27efb2dda8bca8582ba87bf45dc8b3c10c8
Alejo <alejoboga20@gmail.com>
2020-01-29 16:41:50 -0300

    Setting web Pack for CSS
    Adding css-loader and style-loader

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecissionApp';
+import './styles/styles.css';
   
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
   
